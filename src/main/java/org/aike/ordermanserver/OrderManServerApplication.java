84397e5427eafb973a174bc8954c21a160a7ee1a
aike <121231233@123.com>
2025-07-03 00:15:29 +0800

    Initial commit

---
+++ src/main/java/org/aike/ordermanserver/OrderManServerApplication.java
@@ -0,0 +1,13 @@
+package org.aike.ordermanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aike/ordermanserver/OrderManServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aike.ordermanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
